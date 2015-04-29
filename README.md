605ac57c0f7f20560f2122073a8333ee1ee9bc23
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-04-29 14:52:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# lupapiste-commons
+
+Common domain code and resources for lupapiste and lupapiste-toj
+
+## License
+
+Copyright © 2015 Solita
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject lupapiste-commons "0.1.0-SNAPSHOT"
+  :description "Common domain code and resources for lupapiste and lupapiste-toj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
