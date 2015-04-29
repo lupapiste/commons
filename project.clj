76c3d97c3b934b19869f602f7915be5a5045b906
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-04-29 15:55:54 +0300

    Add group-id, deployment configuration

--- project.clj
+++ project.clj
@@ -1,5 +1,14 @@
-(defproject lupapiste-commons "0.1.0-SNAPSHOT"
+(defproject lupapiste/commons "0.1.0-SNAPSHOT"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
+  :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :scm {:url "https://deus.solita.fi/Solita/code/files/lupapiste/repositories/lupapiste-commons/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :deploy-repositories {"snapshots" {:url ***REMOVED***
+                                     :username ***REMOVED***
+                                     :password ***REMOVED***}
+                        "releases" {:url ***REMOVED***
+                                    :username ***REMOVED***
+                                    :password ***REMOVED***
+                                    :sign-releases false}})
