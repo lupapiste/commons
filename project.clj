a1e780f1ac095ff4db56ea20afc4e3b6dc01096d
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-04-30 22:28:23 +0300

    Hack to offer attachment_types namespace as portable source (.cljc)
    Allows use in ClojureScript directly, as long as we refrain from using
    JVM platform specific code.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+(def portable-source "src/lupapiste_commons/attachment_types.clj")
+
 (defproject lupapiste/commons "0.1.0-SNAPSHOT"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
@@ -5,6 +7,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "https://deus.solita.fi/Solita/code/files/lupapiste/repositories/lupapiste-commons/"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  ;; Offer portable source as .cljc in the jar file
+  :filespecs [{:type :bytes
+               :path ~(str (.replaceFirst portable-source "^src/" "") "c")
+               :bytes ~(slurp portable-source)}]
+
   :deploy-repositories {"snapshots" {:url ***REMOVED***
                                      :username ***REMOVED***
                                      :password ***REMOVED***}
