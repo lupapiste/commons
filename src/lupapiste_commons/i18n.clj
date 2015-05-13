5ea96825afe3891a771be6e00428bcd0f9f23b58
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-05-13 13:17:37 +0300

    Strings are now behind :translations -key

--- src/lupapiste_commons/i18n.clj
+++ src/lupapiste_commons/i18n.clj
@@ -5,10 +5,11 @@
 (defn read-translations []
   (resources/txt->map (io/resource "translations.txt")))
 
-(defn keys-by-language [translations & {:keys [str-keys]
-                                        :or {str-keys true}}]
+(defn keys-by-language [{:keys [translations]} & {:keys [str-keys]
+                                           :or {str-keys true}}]
   (reduce (fn [acc [key langs]]
-            (reduce (fn [acc [lang string]] (assoc-in acc [lang (if str-keys (str key) key)] string))
+            (reduce (fn [acc [lang string]]
+                      (assoc-in acc [lang (if str-keys (str key) key)] string))
                     acc
                     langs))
           {}
