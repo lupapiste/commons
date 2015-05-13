06736af37b3d61d8beb181892faf5a198d621703
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-05-13 15:34:25 +0300

    Replace missing translations with default language

--- src/lupapiste_commons/i18n.clj
+++ src/lupapiste_commons/i18n.clj
@@ -1,9 +1,30 @@
 (ns lupapiste-commons.i18n
   (:require [clojure.java.io :as io]
+            [flatland.ordered.map :refer [ordered-map]]
             [lupapiste-commons.i18n-resources :as resources]))
 
-(defn read-translations []
-  (resources/txt->map (io/resource "translations.txt")))
+(def default-lang :fi)
+
+(defn missing-translations? [strings]
+  (seq (filter (fn [[_ v]] (empty? v))
+               (dissoc strings default-lang))))
+
+(defn replace-with-default-lang [strings]
+  (into {} (for [[k v] strings]
+             [k (if (empty? v) (get strings default-lang) v)])))
+
+(defn read-translations
+  ([] (read-translations (io/resource "translations.txt")))
+  ([input]
+   (let [{:keys [translations languages]} (resources/txt->map input)]
+     (reduce (fn [acc [key strings]]
+               (assoc acc
+                      key
+                      (if (missing-translations? strings)
+                        (replace-with-default-lang strings)
+                        strings)))
+             (ordered-map)
+             translations))))
 
 (defn keys-by-language [{:keys [translations]} & {:keys [str-keys]
                                            :or {str-keys true}}]
