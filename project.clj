9689aa79ff845b8762be24413ec14e3721d18705
Markus Penttila <markus.penttila@solita.fi>
2015-08-11 12:24:19 +0300

    Add "edit" and "cancel" translations, bump version to 0.5.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.5.5"
+(defproject lupapiste/commons "0.5.6-SNAPSHOT"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
