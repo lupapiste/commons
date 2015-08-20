cf5ee904d6dc83d01a065758207bd0bde4d000c9
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-08-20 12:43:35 +0300

    Update Timbre to 4.1.1
    Removes shutdown hook that calls (shutdown-agents), so logging inside
    a shutdown hook might work now.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.flatland/ordered "1.5.3"]
                  [ring/ring-core "1.4.0"]
                  [com.stuartsierra/component "0.2.3"]
-                 [com.taoensso/timbre "4.0.2"]
+                 [com.taoensso/timbre "4.1.1"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  [prismatic/schema "0.4.3"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.8.0"]]
