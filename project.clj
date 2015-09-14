5659bc71a1cbf8b33135046299e0ade095a008fc
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-09-14 08:49:55 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.5.8-SNAPSHOT"
+(defproject lupapiste/commons "0.5.9-SNAPSHOT"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
