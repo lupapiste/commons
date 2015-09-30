7e4760cb1fa7f13b298f42c401048c3e9b56b9a4
Tommi Reinikainen <Tommi.Reinikainen@solita.fi>
2015-09-30 15:49:13 +0300

    New icon font

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.5.12"
+(defproject lupapiste/commons "0.5.13"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
