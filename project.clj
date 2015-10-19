8bcd513731c7e7caac555a768395bd506eb228c6
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-10-19 09:17:03 +0300

    Unified state names to camelCase

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.5.19"
+(defproject lupapiste/commons "0.5.20"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
