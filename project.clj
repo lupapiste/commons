7e301818b54b4cae57472e99f9dd49b6b15e88f7
Markus Penttila <markus.penttila@solita.fi>
2016-01-27 16:52:27 +0200

    Add retention and security period end to archive metadata schema

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.6.1"
+(defproject lupapiste/commons "0.6.2"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
