6a9f49b6a8f95c20f50c2e302ca1d7a9f6fe8bff
Markus Rauhala <markus.rauhala@solita.fi>
2016-02-11 15:30:08 +0200

    Version up to 0.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.6.0"
+(defproject lupapiste/commons "0.6.1"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
