f169d629a0e853e2287435aa10100dc246b8d994
Vespe Savikko <vespe.savikko@solita.fi>
2016-02-15 09:47:17 +0200

    One translation (paatosote).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.6.5"
+(defproject lupapiste/commons "0.6.51"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
