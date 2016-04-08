141df12ff6cb94ca45a8bb378ac5012784fbaa5e
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2016-04-08 08:01:43 +0300

    add info localizations and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.21"
+(defproject lupapiste/commons "0.7.22"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
