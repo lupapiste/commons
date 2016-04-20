6dcf3e7df9a3dc5197bfa65d2efda2d0dc85c426
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-04-20 10:14:55 +0300

    Upgrade dependency minor versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.23"
+(defproject lupapiste/commons "0.7.24"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
@@ -6,18 +6,18 @@
             :distribution :repo}
   :scm {:url "https://github.com/lupapiste/commons.git"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [ontodev/excel "0.2.4" :exclusions [xml-apis org.apache.poi/poi-ooxml]]
-                 [org.apache.poi/poi-ooxml "3.13"]
+                 [org.apache.poi/poi-ooxml "3.14"]
                  [org.flatland/ordered "1.5.3"]
                  [ring/ring-core "1.4.0"]
                  [com.stuartsierra/component "0.3.1"]
-                 [com.taoensso/timbre "4.2.1"]
-                 [org.clojure/tools.nrepl "0.2.12"]
+                 [com.taoensso/timbre "4.2.1"]                
                  [prismatic/schema "1.0.4"]
-                 [org.slf4j/slf4j-log4j12 "1.7.14"]
+                 [org.slf4j/slf4j-log4j12 "1.7.21"]
                  [org.apache.pdfbox/pdfbox "2.0.0-RC3"]
                  [org.apache.pdfbox/pdfbox-tools "2.0.0-RC3"]
-                 [com.levigo.jbig2/levigo-jbig2-imageio "1.6.3"]
+                 [com.levigo.jbig2/levigo-jbig2-imageio "1.6.5"]
                  [com.github.jai-imageio/jai-imageio-core "1.3.1"]
                  [com.github.jai-imageio/jai-imageio-jpeg2000 "1.3.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.8.0"]]
