dec476cf3c440ae56845cf23781c5ac6414918e6
Markus Rauhala <markus.rauhala@solita.fi>
2016-06-02 12:59:54 +0300

    Footer text fix.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.31"
+(defproject lupapiste/commons "0.7.32"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
