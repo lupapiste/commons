51e40a67ef97343245fc693f57d5dc36dd3e8afc
Erno Kuusela <erno.kuusela@solita.fi>
2016-08-08 10:09:09 +0300

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.37"
+(defproject lupapiste/commons "0.7.38"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
