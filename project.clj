6073e5ddda099e6a7d28a7205ad123040efefb9b
Antti Slunga <Antti.Slunga@solita.fi>
2016-09-21 13:34:33 +0300

    Project version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.47"
+(defproject lupapiste/commons "0.7.48"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
