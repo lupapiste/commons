494e7102ae19059e4ed7194b0c20e99117fe96da
Markus Rauhala <markus.rauhala@solita.fi>
2016-10-31 09:48:56 +0200

    Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.55"
+(defproject lupapiste/commons "0.7.56"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
