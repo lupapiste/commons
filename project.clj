499ea82b7b9073f96da5f520ae4f3960bc6859d2
Ville Outamaa <ville.outamaa@solita.fi>
2016-11-18 10:11:19 +0200

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.57"
+(defproject lupapiste/commons "0.7.58"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
