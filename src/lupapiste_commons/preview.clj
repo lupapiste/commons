e1cf67721765ac7bcf276d4b42eca304615bcabb
Markus Penttila <markus.penttila@solita.fi>
2016-12-05 17:40:47 +0200

    Render PDF previews directly to small size instead of doing a 2x and then scale down
    This should help somewhat with the preview rendering memory consumption.

--- src/lupapiste_commons/preview.clj
+++ src/lupapiste_commons/preview.clj
@@ -19,25 +19,36 @@
     (ImageIOUtil/writeImage image "jpg" output 72 0.5)
     (ByteArrayInputStream. (.toByteArray output))))
 
+(defn- size-ok? [^BufferedImage image]
+  (if (and (< (.getWidth image) 45000) (< (.getHeight image) 45000))
+    true
+    (do
+      (warnf "Image size (%d x %d) is too big for preview [byte array length exceeds MAX_INTEGER]" (.getWidth image) (.getHeight image))
+      false)))
+
 (defn- ^BufferedImage scale-image
   "Crops and scales BufferedImage to predefined resolution"
   [^BufferedImage image]
-  (let [original-height (.getHeight image)
-        original-width (.getWidth image)
-        crop-x (if (< (/ original-height original-width) 5/7) (- original-width (/ original-height 5/7)) 0)
-        crop-y (if (< (/ original-width original-height) 5/7) (- original-height (/ original-width 5/7)) 0)
-        scale (min (/ rez (- original-width crop-x)) (/ rez (- original-height crop-y)))
-        width (* scale (- original-width crop-x))
-        height (* scale (- original-height crop-y))
-        new-image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
-    (debugf "scale-image rez: %s x %s, crop: %s x %s, scale by %s" original-width original-height crop-x crop-y scale)
-    (doto (.createGraphics new-image)
-      (.setRenderingHint RenderingHints/KEY_INTERPOLATION, RenderingHints/VALUE_INTERPOLATION_BICUBIC)
-      (.setRenderingHint RenderingHints/KEY_RENDERING, RenderingHints/VALUE_RENDER_QUALITY)
-      (.setRenderingHint RenderingHints/KEY_ANTIALIASING, RenderingHints/VALUE_ANTIALIAS_ON)
-      (.drawImage image, 0, 0, width, height, crop-x, crop-y, original-width, original-height, nil)
-      (.dispose))
-    new-image))
+  (when (size-ok? image)
+    (let [original-width (.getWidth image)
+          original-height (.getHeight image)
+          crop-x (if (< (/ original-height original-width) 5/7) (- original-width (/ original-height 5/7)) 0)
+          crop-y (if (< (/ original-width original-height) 5/7) (- original-height (/ original-width 5/7)) 0)
+          scale (min (/ rez (- original-width crop-x)) (/ rez (- original-height crop-y)))
+          width (* scale (- original-width crop-x))
+          height (* scale (- original-height crop-y))
+          new-image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
+      (debugf "scale-image rez: %s x %s, crop: %s x %s, scale: %s" original-width original-height crop-x crop-y scale)
+      (doto (.createGraphics new-image)
+        (.setRenderingHint RenderingHints/KEY_INTERPOLATION, RenderingHints/VALUE_INTERPOLATION_BICUBIC)
+        (.setRenderingHint RenderingHints/KEY_RENDERING, RenderingHints/VALUE_RENDER_QUALITY)
+        (.setRenderingHint RenderingHints/KEY_ANTIALIASING, RenderingHints/VALUE_ANTIALIAS_ON)
+        (.drawImage image, 0, 0, width, height, crop-x, crop-y, original-width, original-height, nil)
+        (.dispose))
+      new-image)))
+
+(defn- must-crop? [width height]
+  (not (< 5/7 (/ width height) 7/5)))
 
 (defn- ^BufferedImage pdf-to-buffered-image
   "Converts 1. page from PDF to BufferedImage"
@@ -46,12 +57,25 @@
                 (FileInputStream. pdf-input)
                 pdf-input)]
     (with-open [document (PDDocument/load input (MemoryUsageSetting/setupMixed (* 100 1024 1024)))]
-      (.renderImage (PDFRenderer. document) 0 2))))
+      (let [crop-box (-> (.getPage document 0) (.getCropBox))
+            original-width (.getWidth crop-box)
+            original-height (.getHeight crop-box)
+            ; If the image is too wide / high, we have to crop it to a more manageable aspect ratio
+            ; and because the PDFRenderer does not directly support this, we first render in 2 x target resolution
+            ; and then scale the image down to the final target size (with cropping)
+            crop? (must-crop? original-width original-height)
+            target-rez (if crop? (* 2 rez) rez)
+            scale (->> (max original-width original-height) (/ target-rez) float)]
+        (debugf "scale for pdf preview: %s" scale)
+        (cond-> (PDFRenderer. document)
+                :always (.renderImage 0 (float scale))
+                crop? scale-image)))))
 
 (defn- ^BufferedImage raster-to-buffered-image
   "Converts Raster image to BufferedImage"
   [input]
-  (ImageIO/read ^InputStream (io/input-stream input)))
+  (-> (ImageIO/read ^InputStream (io/input-stream input))
+      scale-image))
 
 (defn converter [content-type]
   (cond
@@ -66,18 +90,10 @@
       (op content))
     (catch Exception e (warnf "preview to-buffered-image was unable to read content of a %s file: %s" content-type e))))
 
-(defn- size-ok? [^BufferedImage image content-type]
-  (if (and (< (.getWidth image) 45000) (< (.getHeight image) 45000))
-    true
-    (do
-      (warnf "Image type [%s] size (%d x %d) is too big for preview [byte array length exceeds MAX_INTEGER]" content-type (.getWidth image) (.getHeight image))
-      false)))
-
 (defn create-preview
   "Tries to create preview image IF content type can be processed to image by JAI or apache.pdfbox. Retuns nil on fail"
   [content content-type]
-  (when-let [image (to-buffered-image content content-type)]
-    (when (size-ok? image content-type)
-      (buffered-image-to-input-stream (scale-image image)))))
+  (some-> (to-buffered-image content content-type)
+          buffered-image-to-input-stream))
 
 (defn placeholder-image [] (ByteArrayInputStream. (byte-array 0)))
