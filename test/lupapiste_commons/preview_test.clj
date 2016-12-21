7abd7c5fd9a28c95494d6f37afa24c767329d889
Markus Penttila <markus.penttila@solita.fi>
2016-12-21 17:13:51 +0200

    Fix tests

--- test/lupapiste_commons/preview_test.clj
+++ test/lupapiste_commons/preview_test.clj
@@ -6,7 +6,9 @@
 
 (def pdf-1 "dev-resources/test-pdf.pdf")
 (def jpg-1 "resources/preview-sample-1.jpg")
+(def jpg-2 "resources/preview-sample-2.jpg")
 (def image-1 (ImageIO/read (io/resource jpg-1)))
+(def image-2 (ImageIO/read (io/resource jpg-2)))
 
 (deftest to-buffered-image-fails-quietly
   (is (nil? (#'preview/to-buffered-image nil "application/pdf")))
@@ -17,14 +19,19 @@
 (deftest pdf-to-buffered-image-works
   (let [img (#'preview/pdf-to-buffered-image pdf-1)]
     (is (= (.getHeight img) 600))
-    (is (= (.getWidth img) 428))))
+    (is (= (.getWidth img) 424))))
 
 (deftest scale-image-works
-  (is (= (.getWidth (#'preview/scale-image image-1)) 428)))
+  (is (= (.getWidth (#'preview/scale-image image-1)) 424)))
+
+(deftest image-cropping-works
+  (let [scaled (#'preview/scale-image image-2)]
+    (is (= (.getWidth scaled) 419))
+    (is (= (.getHeight scaled) 600))))
 
 (deftest buffered-image-to-input-stream-returns-correct-stream
   ;; NB: changing preview compression fails this test
-  (is (= (.available (#'preview/buffered-image-to-input-stream image-1)) 667967)))
+  (is (= (.available (#'preview/buffered-image-to-input-stream image-1)) 811937)))
 
 (deftest pdf-to-buffered-image-works-with-jbig2
   (is (= (.getWidth (#'preview/pdf-to-buffered-image "dev-resources/jbig2.pdf")) 600)))
