9dc99b58709037b988b2ca570e4d1d162f645d9b
Antti Rämö <antti.ramo@solita.fi>
2017-01-02 13:47:07 +0200

    Bump version to 0.7.68

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.67"
+(defproject lupapiste/commons "0.7.68"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
