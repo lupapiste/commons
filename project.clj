7d126f20825d80cb4ea9b4dcfb9c101551fa0a75
Vespe Savikko <vespe.savikko@solita.fi>
2017-01-16 13:48:21 +0200

    Lupicons font update.
    Document plus and lock open icons.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.72"
+(defproject lupapiste/commons "0.7.73"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
