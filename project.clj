cad1fcb536b2b776e99b9a7ea2f902ee630deb30
Markus Penttila <markus.penttila@solita.fi>
2017-01-22 12:09:07 +0200

    Upgrade dependencies, move ring and component to provided scope.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.76"
+(defproject lupapiste/commons "0.7.77"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
@@ -10,17 +10,18 @@
                  [ontodev/excel "0.2.4" :exclusions [xml-apis org.apache.poi/poi-ooxml]]
                  [org.apache.poi/poi-ooxml "3.15"]
                  [org.flatland/ordered "1.5.4"]
-                 [ring/ring-core "1.5.0"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [com.taoensso/timbre "4.7.4"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [com.taoensso/timbre "4.8.0"]
                  [prismatic/schema "1.1.3"]
-                 [org.slf4j/slf4j-log4j12 "1.7.21"]
-                 [org.apache.pdfbox/pdfbox "2.0.3"]
-                 [org.apache.pdfbox/pdfbox-tools "2.0.3"]
+                 [org.slf4j/slf4j-log4j12 "1.7.22"]
+                 [org.apache.pdfbox/pdfbox "2.0.4"]
+                 [org.apache.pdfbox/pdfbox-tools "2.0.4"]
                  [com.github.jai-imageio/jai-imageio-core "1.3.1"]
                  [com.github.jai-imageio/jai-imageio-jpeg2000 "1.3.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.8.0"]]
   :profiles {:dev {:dependencies [[flare "0.2.9"]]
                    :injections [(require 'flare.clojure-test)
-                                (flare.clojure-test/install!)]}}
+                                (flare.clojure-test/install!)]}
+             :provided {:dependencies [[com.stuartsierra/component "0.3.2"]
+                                       [ring/ring-core "1.5.1"]]}}
   :cljsbuild {:builds {:dev {:source-paths ["src"]}}})
