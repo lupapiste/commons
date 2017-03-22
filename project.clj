883f9ff869f3f12479f1a852669b3fddd6687a9c
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-03-22 17:15:15 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.92"
+(defproject lupapiste/commons "0.7.93"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
