d453481bac99b00a84376cb5d41a4b5129ed9c40
Antti Salmivaara <antti.salmivaara@solita.fi>
2017-06-13 13:50:54 +0300

    update translations

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject lupapiste/commons "0.7.108"
+(defproject lupapiste/commons "0.7.109"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
