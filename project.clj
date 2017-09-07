7eeb5e548923d2e1b5b0d888f935cdfb605d2ace
Markus Penttila <markus.penttila@evolta.fi>
2017-09-07 15:05:35 +0300

    Update dependencies, set version to 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.7.121"
+(defproject lupapiste/commons "0.8.0"
   :description "Common domain code and resources for lupapiste and lupapiste-toj"
   :url "http://www.solita.fi"
   :license {:name "European Union Public License"
@@ -8,14 +8,14 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [ontodev/excel "0.2.4" :exclusions [xml-apis org.apache.poi/poi-ooxml]]
-                 [org.apache.poi/poi-ooxml "3.15"]
-                 [org.flatland/ordered "1.5.4"]
+                 [org.apache.poi/poi-ooxml "3.16"]
+                 [org.flatland/ordered "1.5.6"]
                  [com.stuartsierra/component "0.3.2"]
-                 [com.taoensso/timbre "4.8.0"]
-                 [prismatic/schema "1.1.3"]
-                 [org.slf4j/slf4j-log4j12 "1.7.22"]
-                 [org.apache.pdfbox/pdfbox "2.0.4"]
-                 [org.apache.pdfbox/pdfbox-tools "2.0.4"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [prismatic/schema "1.1.6"]
+                 [org.slf4j/slf4j-log4j12 "1.7.25"]
+                 [org.apache.pdfbox/pdfbox "2.0.7"]
+                 [org.apache.pdfbox/pdfbox-tools "2.0.7"]
                  [com.github.jai-imageio/jai-imageio-core "1.3.1"]
                  [com.github.jai-imageio/jai-imageio-jpeg2000 "1.3.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.8.0"]]
@@ -23,5 +23,5 @@
                    :injections [(require 'flare.clojure-test)
                                 (flare.clojure-test/install!)]}
              :provided {:dependencies [[com.stuartsierra/component "0.3.2"]
-                                       [ring/ring-core "1.5.1"]]}}
+                                       [ring/ring-core "1.6.2"]]}}
   :cljsbuild {:builds {:dev {:source-paths ["src"]}}})
