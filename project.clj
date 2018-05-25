ba1ddb9bd826193620d315d4217e11ac80e19b2b
Markus Rauhala <markus.rauhala@evolta.fi>
2018-05-25 11:23:10 +0300

    Version bump 0.9.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.14"
+(defproject lupapiste/commons "0.9.15"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
