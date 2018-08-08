43d79b22ac361c459448bee7777b9710bd71fe23
Markus Penttila <markus.penttila@evolta.fi>
2018-08-08 16:58:26 +0300

    Add translations for coordinate display

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.20"
+(defproject lupapiste/commons "0.9.21"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
