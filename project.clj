0abcb4bf79c680ddf5865159d2fbe8538ab0a592
Antti Salmivaara <antti.salmivaara@evolta.fi>
2018-08-09 11:52:31 +0300

    Commons to 0.9.22

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.21"
+(defproject lupapiste/commons "0.9.22"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
