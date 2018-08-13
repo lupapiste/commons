70bcc0b146d7f6cff78659e1bd9bbcc98e853908
Markus Rauhala <markus.rauhala@evolta.fi>
2018-08-13 15:31:40 -0400

    Asemapiirros translation, commons to 0.9.23

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.22"
+(defproject lupapiste/commons "0.9.23"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
