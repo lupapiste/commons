15d369c196f5b4680fcf08acbacd78059e6c8484
Antti Slunga <Antti.Slunga@solita.fi>
2018-11-15 15:27:09 +0200

    PATE-184 Verdict proposal attachment type

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.28"
+(defproject lupapiste/commons "0.9.29"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
