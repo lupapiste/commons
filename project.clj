8a7f5c30e68bc5313d2a4a20cdbdebe1f8736bc8
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-11-15 17:16:31 +0200

    PATE-150 Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.29"
+(defproject lupapiste/commons "0.9.30"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
