201779a42c40c9289bf1f1e4243501e61057b6cb
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-12-10 16:25:00 +0200

    Add localizations related to PATE-109

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.36"
+(defproject lupapiste/commons "0.9.37"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
