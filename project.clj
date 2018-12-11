5a4b9e914d988a744ac74693b394870f920c419f
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-12-11 13:04:46 +0200

    PATE-109 Add missing localizations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.39"
+(defproject lupapiste/commons "0.9.40"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
