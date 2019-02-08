c6fd0e8735f275850621e243978c01f284e92586
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-02-08 09:42:42 +0200

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.46"
+(defproject lupapiste/commons "0.9.47"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
