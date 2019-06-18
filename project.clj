c8b5715fdb80cdb82fb55fe8e1f4be91ed0c7fe7
pisketti <tero.kadenius@flowa.fi>
2019-06-18 13:49:47 +0300

    LPK-4728 adds localisations for sending invoices to billing system
    Also updates the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.9.62"
+(defproject lupapiste/commons "0.9.63"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
