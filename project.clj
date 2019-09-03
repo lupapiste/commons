f9d011dbfba0bf62d753afde27f3c93097e8135e
pisketti <tero.kadenius@flowa.fi>
2019-09-03 09:29:13 +0300

    LPK-4785 Adds localization to non billable work days label to be used in authority invoice tab

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.10.0"
+(defproject lupapiste/commons "0.10.1"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
