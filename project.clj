ddeb787dc49ec3af76d1ed3524510391d5f5bb55
Jari Asp <jari.asp@solita.fi>
2019-11-20 14:08:05 +0200

    LPK-5115: Better naming.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.10.2"
+(defproject lupapiste/commons "0.10.3"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
