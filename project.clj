beb84712a2d67276724d4814208bf9f63783b909
Timo Mihaljov <timo.mihaljov@solita.fi>
2019-11-20 16:21:48 +0200

    LPK-5127 Add new units for hour and week

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.10.3"
+(defproject lupapiste/commons "0.10.4"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
