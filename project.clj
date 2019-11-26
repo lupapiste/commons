e15729530811bf43020497cb6763a45fa0ac60c7
Jari Asp <jari.asp@solita.fi>
2019-11-26 14:59:43 +0200

    LPK-5140: Removed not-needed translations.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.10.4"
+(defproject lupapiste/commons "0.10.5"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
