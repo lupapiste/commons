58e03e9d7ef3fd7b9ea690a14fe304d8ea9fb2c2
Markus Rauhala <markus.rauhala@evolta.fi>
2020-03-13 13:47:02 +0200

    LPK-4052 updated icon font.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.10.14"
+(defproject lupapiste/commons "0.10.15"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.evolta.fi"
   :license {:name "Eclipse Public License"
