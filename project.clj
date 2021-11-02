78ab66ae12635068b308107cf0afd8993eb73867
Aarne Uotila <aarne.uotila@cloudpermit.com>
2021-11-02 21:15:55 +0200

    LPK-5898 Add kuvaus attachment type

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.11.02"
+(defproject lupapiste/commons "0.11.03"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
