1dc18b1fb877bc582c10a6cf31e3f8af538641b0
Markus Penttila <markus.penttila@cloudpermit.com>
2021-11-23 19:56:58 +0200

    Add new footer links, add new Lupapiste logo as SVG

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.11.04"
+(defproject lupapiste/commons "0.11.5"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
