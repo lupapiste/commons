21bb00a96cdcb90ede59c12a46670d39a6246476
Markus Penttila <markus.penttila@cloudpermit.com>
2021-12-27 21:07:25 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -5,17 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :scm {:url "https://github.com/lupapiste/commons.git"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.nrepl "0.2.13"]
-                 [org.clojure/core.memoize "0.7.1"]
+                 [org.clojure/core.memoize "1.0.253"]
                  [ontodev/excel "0.2.4" :exclusions [xml-apis org.apache.poi/poi-ooxml]]
                  [org.apache.poi/poi-ooxml "3.16"]
-                 [org.flatland/ordered "1.5.7"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [prismatic/schema "1.1.7"]
-                 [com.github.jai-imageio/jai-imageio-core "1.3.1"]
-                 [com.github.jai-imageio/jai-imageio-jpeg2000 "1.3.0"]
-                 [clj-http "3.8.0"]]
+                 [org.flatland/ordered "1.15.10"]
+                 [com.taoensso/timbre "5.1.2"]
+                 [prismatic/schema "1.2.0"]
+                 [com.github.jai-imageio/jai-imageio-core "1.4.0"]
+                 [com.github.jai-imageio/jai-imageio-jpeg2000 "1.4.0"]
+                 [clj-http "3.12.3"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.8.0"]]
   :profiles {:dev {:dependencies [[flare "0.2.9"]]
                    :injections [(require 'flare.clojure-test)
