eecbc0427df435308d399fb4e788cc98a977d69a
Vespe Savikko <vespe.savikko@cloudpermit.com>
2022-01-17 14:56:49 +0200

    Translations.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.12.0"
+(defproject lupapiste/commons "0.12.1"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
