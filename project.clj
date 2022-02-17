813212e5afaca7d56214e58b68eeed7c3f3f4fd0
Aarne Uotila <aarne.uotila@cloudpermit.com>
2022-02-17 18:31:07 +0200

    Revert "LPK-5816 Fix ymparistoluvan-selventaminen state machine order"
    This reverts commit 7060b0c9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "0.12.8"
+(defproject lupapiste/commons "0.12.7"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
