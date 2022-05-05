cc572512ead97286cf016f470477e856be022b94
ilmirajat <aplappi@gmail.com>
2022-05-05 12:39:39 +0300

    LPK-6235 add translations for Leirintaalueilmoitus  (#20)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "1.0.0"
+(defproject lupapiste/commons "1.0.1"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
