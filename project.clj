873ec7627ddf5af584aad577efa327a862cc58dd
Ari-Pekka Lappi <aplappi@gmail.com>
2022-05-12 10:00:32 +0300

    LPK-5831 Add sv translation for Rakennekuva

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "1.0.3"
+(defproject lupapiste/commons "1.0.4"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
