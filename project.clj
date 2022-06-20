176de49ede916cfe23d25596303afe30f9d5225c
Ari-Pekka Lappi <aplappi@gmail.com>
2022-06-20 10:25:46 +0300

    Add attachment type translations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "1.0.16"
+(defproject lupapiste/commons "1.0.17"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
