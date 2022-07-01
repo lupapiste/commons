fff0cf2e6645b44f9247adb9d5bd98f44d2a7bd2
Ari-Pekka Lappi <aplappi@gmail.com>
2022-07-01 17:14:10 +0300

    Removes (probably unintented) empty string fallback into default language translation
    - This behavior can be switched back on by using :fallback-to-default-lang flag in read-transtalations functions

--- test/lupapiste_commons/i18n/core_test.clj
+++ test/lupapiste_commons/i18n/core_test.clj
@@ -4,11 +4,20 @@
             [clojure.test :refer :all]
             [lupapiste-commons.i18n.core :refer :all]))
 
-(deftest use-default-lang
-  (testing "String from default language used if no translation present"
+(deftest read-translations
+  (testing ":fallback-to-default-lang flag treats empty strings as missing value
+            and replaces them with default language resouces value"
     (let [input [["label" "fi" "moi"]
                  ["label" "sv" ""]]
           string (s/join "\n" (map #(s/join " " (mapv pr-str %)) input))]
       (is (= {:languages [:fi :sv]
               :translations {'label {:fi "moi" :sv "moi"}}}
+             (read-translations (.getBytes string) :fallback-to-default-lang true)))))
+
+  (testing "By default empty string are not considered as missing translations"
+    (let [input [["label" "fi" "moi"]
+                 ["label" "sv" ""]]
+          string (s/join "\n" (map #(s/join " " (mapv pr-str %)) input))]
+      (is (= {:languages [:fi :sv]
+              :translations {'label {:fi "moi" :sv ""}}}
              (read-translations (.getBytes string)))))))
