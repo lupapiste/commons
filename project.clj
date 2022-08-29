ecbd458366ea1fd7964bdc2006fb2b36cb3d264e
ilmirajat <aplappi@gmail.com>
2022-08-29 17:39:25 +0300

    Apply sv translations dated 2022-08-22 (#28)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "3.1.1"
+(defproject lupapiste/commons "3.1.2"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
