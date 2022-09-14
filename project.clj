c88ecc6ff89650467a0aa1a7f473ccdfd882690b
Ari-Pekka Lappi <aplappi@gmail.com>
2022-09-14 13:24:09 +0300

    LPK-6260 Add fi and en texts for lannan-varastointi-v2 operation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "3.1.4"
+(defproject lupapiste/commons "3.1.5"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
