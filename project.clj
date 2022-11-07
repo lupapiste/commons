d83164db6cb405b1f0dc717417b3964484444f08
Ari-Pekka Lappi <aplappi@gmail.com>
2022-11-07 12:51:53 +0200

    Update SV translations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "3.1.5"
+(defproject lupapiste/commons "3.1.8"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
