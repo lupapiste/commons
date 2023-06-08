2342ab28e10c00d9a1df349677ba9b11f07a1ea7
Markus Penttila <markus.penttila@cloudpermit.com>
2023-06-08 19:34:15 -0400

    Add X-Content-Type-Options and Referrer-Policy to index headers

--- src/lupapiste_commons/route_utils.clj
+++ src/lupapiste_commons/route_utils.clj
@@ -4,21 +4,23 @@
 
 (defonce index-response (atom nil))
 
-(defn process-index-response [{:strs [git-commit]}]
+(defn process-index-response [{:keys [git-commit]}]
   (if @index-response
     @index-response
     (with-open [is (-> (io/resource "public/index.html")
                        (io/input-stream))]
-      (let [content (slurp is)
-            version (if git-commit (subs git-commit 0 7) "dev")
+      (let [content       (slurp is)
+            version       (if git-commit (subs git-commit 0 7) "dev")
             replaced-html (-> (string/replace content ".css" (str ".css?v=" version))
                               (string/replace ".js" (str ".js?v=" version)))
-            response {:status 200
-                      :body replaced-html
-                      :headers {"Content-Type" "text/html"
-                                "Content-Length" (str (count replaced-html))
-                                "Cache-Control" "no-cache, no-store, must-revalidate"
-                                "Pragma" "no-cache"
-                                "Expires" "0"}}]
+            response      {:status  200
+                           :body    replaced-html
+                           :headers {"Content-Type"           "text/html"
+                                     "Content-Length"         (str (count replaced-html))
+                                     "X-Content-Type-Options" "nosniff"
+                                     "Referrer-Policy"        "no-referrer"
+                                     "Cache-Control"          "no-cache, no-store, must-revalidate"
+                                     "Pragma"                 "no-cache"
+                                     "Expires"                "0"}}]
         (reset! index-response response)
         response))))
