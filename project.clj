76590a5481f8b9bbe39446e3ed7dcfb45c08951e
Markus Penttila <markus.penttila@cloudpermit.com>
2023-06-08 21:27:34 -0400

    Move timbre to provided dependencies to avoid conflicts

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "5.1.0"
+(defproject lupapiste/commons "5.1.1"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name "Eclipse Public License"
@@ -10,7 +10,6 @@
                  [org.clojure/core.memoize "1.0.253"]
                  [dk.ative/docjure "1.18.0"]
                  [org.flatland/ordered "1.15.10"]
-                 [com.taoensso/timbre "5.1.2"]
                  [prismatic/schema "1.4.1"]
                  [clj-http "3.12.3"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.8.0"]]
@@ -18,5 +17,6 @@
                    :injections [(require 'flare.clojure-test)
                                 (flare.clojure-test/install!)]}
              :provided {:dependencies [[com.stuartsierra/component "0.3.2"]
-                                       [ring/ring-core "1.9.6"]]}}
+                                       [ring/ring-core "1.9.6"]
+                                       [com.taoensso/timbre "5.1.2"]]}}
   :cljsbuild {:builds {:dev {:source-paths ["src"]}}})
