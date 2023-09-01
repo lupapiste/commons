b630fa1ab1dc990b954b51764fa14e591c8fee6b
Vespe Savikko <vespe.savikko@cloudpermit.com>
2023-09-01 16:25:25 +0300

    Konversio-rakennus localizations.
    From the translation term point of view, konversio-rakennus is an
    alias for konversio.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "5.1.3"
+(defproject lupapiste/commons "5.1.4"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name         "Eclipse Public License"
