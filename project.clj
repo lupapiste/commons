9020c02c2adeecc96d9fa6e714d166cedbe39071
Markus Penttila <markus.penttila@cloudpermit.com>
2023-09-15 21:50:08 -0400

    Update favicons

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "5.1.5"
+(defproject lupapiste/commons "5.2.0"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name         "Eclipse Public License"
