f08565689a19ebe059da1eac1e8e19fb5dcc1926
Markus Kankkonen <markus.kankkonen@protonmail.com>
2024-09-23 12:35:22 +0300

    bumpped project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "5.3.2"
+(defproject lupapiste/commons "5.3.3"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name         "Eclipse Public License"
