d3778723f8faac22256af94ae463eff58fe803ce
Marwwin <markus.kankkonen@protonmail.com>
2025-05-26 11:44:59 +0300

    LPK-7315: Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "5.3.14"
+(defproject lupapiste/commons "5.3.15"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name         "Eclipse Public License"
