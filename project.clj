1f786de49164fdd541aef9145584844e9f46931c
Marwwin <markus.kankkonen@protonmail.com>
2025-06-10 10:22:09 +0300

    LPK-7315: Update planning permit in operations.tree

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lupapiste/commons "5.3.15"
+(defproject lupapiste/commons "5.3.16"
   :description "Common domain code and resources for lupapiste applications"
   :url "https://www.lupapiste.fi"
   :license {:name         "Eclipse Public License"
